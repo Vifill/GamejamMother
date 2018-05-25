0ba6b0871790745b3a2912132eb2897d3b6fdd04
Vífill Valdimarsson <vifill12@gmail.com>
2018-05-26 01:41:09 +0200

    Base for the spawn system

---
+++ Gamejam Mother/Assets/_Scripts/SpawnModel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[CreateAssetMenu(menuName = "Configs/Spawn/SpawnModel")]
+public class SpawnModel : ScriptableObject
+{
+    public float SpawnRate;
+    public List<PopupChanceModel> PopupChanceModel;
+}
+
+[Serializable]
+public class PopupChanceModel
+{
+    public GameObject PopupPrefab;
+    public float ChanceOfSpawn;
+}
