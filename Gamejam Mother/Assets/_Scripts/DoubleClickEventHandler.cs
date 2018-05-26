4baab324cb983b0f15b343e04df53e92a55fcb1a
Theodor Te Maiharoa <tedditm@gmail.com>
2018-05-26 13:47:22 +0200

    doubleclick event handler

---
+++ Gamejam Mother/Assets/_Scripts/DoubleClickEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[AddComponentMenu("Event/DoubleClickEvent")]
+public class DoubleClickEventHandler : MonoBehaviour, IPointerClickHandler 
+{
+    public UnityEvent DoubleClickEvent;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.clickCount == 2)
+        {
+            DoubleClickEvent.Invoke();
+            Debug.Log("Double Clicked");
+        }
+    }
+}
