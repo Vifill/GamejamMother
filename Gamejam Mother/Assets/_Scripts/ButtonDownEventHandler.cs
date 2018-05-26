b7ff27514fd1110c6d3b417a72374305e023d81c
Theodor Te Maiharoa <tedditm@gmail.com>
2018-05-26 21:17:00 +0200

    mouse down to close popups now

---
+++ Gamejam Mother/Assets/_Scripts/ButtonDownEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ButtonDownEventHandler : MonoBehaviour, IPointerDownHandler
+{
+    public UnityEvent ButtonDownEvent;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        ButtonDownEvent.Invoke();
+    }
+}
