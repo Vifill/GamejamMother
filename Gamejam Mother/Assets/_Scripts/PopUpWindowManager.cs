8a248538c0011a9e4b6782c758cc0eccdbd22e58
Sindri Freyr <sindridi@gmail.com>
2018-05-27 15:30:17 +0200

    no message

--- Gamejam Mother/Assets/_Scripts/PopUpWindowManager.cs
+++ Gamejam Mother/Assets/_Scripts/PopUpWindowManager.cs
@@ -27,7 +27,10 @@ public class PopUpWindowManager : MonoBehaviour
 
     private void OnClose()
     {
-        GameController.RemovePopup(gameObject);
+        if (gameObject != null)
+        {
+            GameController.RemovePopup(gameObject);
+        }
     }
 
     public void AdButtonClick()
